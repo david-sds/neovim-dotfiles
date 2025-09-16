fe0ca6049600f0991b25583073c52a40012c4042
david-sds <dave.sathler@gmail.com>
2025-09-16 10:46:29 -0300

    triangular lualine alternative

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -10,10 +10,9 @@ return {
 	config = function()
 		require("lualine").setup({
 			options = {
-				-- theme = "melange",
 				icons_enabled = true,
-				-- section_separators = { left = "", right = "" },
-				section_separators = { left = "", right = "" },
+	      section_separators = { left = '', right = ''},
+        -- section_separators = { left = "", right = "" },
 				component_separators = "|",
 			},
 		})
